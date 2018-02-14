5c76b95e92dcc895b43b942d8b6880a7f498e53d
trickyzeeshan <tricky.zeeshan@gmail.com>
2018-02-14 19:20:43 +0530

    Design updating

--- app/src/main/java/com/school/jishan/demoschoolbyjishan/SplashScreen.java
+++ app/src/main/java/com/school/jishan/demoschoolbyjishan/SplashScreen.java
@@ -1,13 +1,34 @@
 package com.school.jishan.demoschoolbyjishan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        Thread thread=new Thread()
+        {
+            public void run()
+            {
+                try {
+                    sleep(3000);
+                    startActivity(new Intent(SplashScreen.this, Login.class));
+                    finish();
+
+                }catch (Exception e)
+                {
+
+                }
+            }
+        };thread.start();
+
     }
 }
